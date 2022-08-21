78424671b23a4617e3c708caa4efe58ffcdd0cfa
trinh quan <quan20028a3kb@gmail.com>
2022-08-21 23:30:21 +0700

    master: repository class

--- src/main/java/com/example/springboot/Repository/ClassRepository.java
+++ src/main/java/com/example/springboot/Repository/ClassRepository.java
@@ -1,7 +1,10 @@
 package com.example.springboot.Repository;
 
+import com.example.springboot.Constant.Major;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ClassRepository extends JpaRepository<Class,Integer> {
+import java.util.Optional;
 
+public interface ClassRepository extends JpaRepository<Class,Integer> {
+    public Optional<Class> findClassByMajor(Major major);
 }
