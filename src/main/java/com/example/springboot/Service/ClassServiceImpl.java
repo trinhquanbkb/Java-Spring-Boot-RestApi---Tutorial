6fdaafadc1d462d1481f7d1a325804905d67bcaa
trinh quan <quan20028a3kb@gmail.com>
2022-08-22 17:09:01 +0700

    service-1: CRUD class

--- src/main/java/com/example/springboot/Service/ClassServiceImpl.java
+++ src/main/java/com/example/springboot/Service/ClassServiceImpl.java
@@ -2,11 +2,14 @@ package com.example.springboot.Service;
 
 import com.example.springboot.Entity.ClassInfo;
 import com.example.springboot.Payload.Request.ClassRequest;
+import com.example.springboot.Payload.Request.ClassUpdateRequest;
 import com.example.springboot.Repository.ClassRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ClassServiceImpl implements ClassService {
@@ -21,5 +24,34 @@ public class ClassServiceImpl implements ClassService {
         classNew.setCreateAt(new Date(System.currentTimeMillis()));
         classNew.setUpdateAt(new Date(System.currentTimeMillis()));
         classRepository.save(classNew);
+    }
+
+    @Override
+    public List<ClassInfo> getAllClass(){
+        return classRepository.findAll();
+    }
+
+    @Override
+    public Optional<ClassInfo> getClassById(int id){
+        return classRepository.findById(id);
+    }
+
+    @Override
+    public void deleteClassById(int id){
+        classRepository.deleteById(id);
+    }
+
+    @Override
+    public void updateClass(ClassUpdateRequest classUpdateRequest){
+        for(ClassInfo classInfo: classRepository.findAll()) {
+            if (classUpdateRequest.getId() == classInfo.getId()) {
+                ClassInfo classNew = new ClassInfo();
+                classNew.setClassName(classUpdateRequest.getClassName());
+                classNew.setMajor(classUpdateRequest.getMajor());
+                classNew.setCreateAt(new Date(System.currentTimeMillis()));
+                classNew.setUpdateAt(new Date(System.currentTimeMillis()));
+                classRepository.save(classNew);
+            }
+        }
     }
 }
